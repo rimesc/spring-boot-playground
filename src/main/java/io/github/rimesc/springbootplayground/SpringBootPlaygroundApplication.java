5af4bb0e9d0eee189131bade420b3d2e0ffb26d0
Chris Rimes <chris.rimes@gmail.com>
2022-05-14 13:57:41 +0100

    Style fixes

--- src/main/java/io/github/rimesc/springbootplayground/SpringBootPlaygroundApplication.java
+++ src/main/java/io/github/rimesc/springbootplayground/SpringBootPlaygroundApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPlaygroundApplication.class, args);
-	}
+  @SuppressWarnings("checkstyle:MissingJavadocMethod")
+  public static void main(final String[] args) {
+    SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+  }
 
 }
--- src/test/java/io/github/rimesc/springbootplayground/SpringBootPlaygroundApplicationTests.java
+++ src/test/java/io/github/rimesc/springbootplayground/SpringBootPlaygroundApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootPlaygroundApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
